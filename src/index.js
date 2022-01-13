b53b579d998a43038fbb582f911a389d11173077
KotenkoNata <kotenkonata8@gmail.com>
2022-01-13 13:59:21 +0200

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let convertToMorse = '';
+
+    let result = '';
+
+    for (let i = 0; i <= expr.length-1; i = i + 10){
+
+        let letter = expr.slice(i,i+10);
+
+        convertToMorse = '';
+
+        for(let k = letter.length; k >= 0; k=k-2){
+
+            let syllable = letter.slice(k-2, k);
+
+            if(syllable === "10"){
+                convertToMorse = "."+convertToMorse;
+            }else if(syllable === "11"){
+                convertToMorse = "-"+convertToMorse;
+            }
+        }
+
+        MORSE_TABLE[convertToMorse] ? result = result + MORSE_TABLE[convertToMorse] : result = result + " ";
+    }
+    return result;
 }
+
+// console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010")); //result = "hello world";
 
 module.exports = {
     decode
